fe5fb0600369c8b662cb13eba45b1140f416e93d
Ger Hobbelt <ger@hobbelt.com>
2023-10-29 21:13:49 +0100

    fix MSVC warning about integer being shifted out of bounds.

--- tests/testcases/hash.cpp
+++ tests/testcases/hash.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 TEST(Hash, BASE){
     unordered_map<uint128_t, string> map;
-    uint128_t a = 1000000 << 32;
+    uint128_t a = 1000000ULL << 32;
     map.insert(make_pair(a, "hello"));
     map.insert(make_pair(1, "bye"));
     ASSERT_EQ(map[a], "hello");
